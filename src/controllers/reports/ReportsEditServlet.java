730152466a3dc86e8079b240e955312fd5e9d91b
miyuki.o <miyukiomaron@gmail.com>
2018-08-02 20:57:34 +0900

    Add Report update

--- src/controllers/reports/ReportsEditServlet.java
+++ src/controllers/reports/ReportsEditServlet.java
@@ -13,12 +13,13 @@ import javax.servlet.http.HttpServletResponse;
 import models.Employee;
 import models.Report;
 import utils.DBUtil;
+
 /**
  * Servlet implementation class ReportsEditServlet
  */
 @WebServlet("/reports/edit")
 public class ReportsEditServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
@@ -28,24 +29,25 @@ public class ReportsEditServlet extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		EntityManager em = DBUtil.createEntityManager();
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        EntityManager em = DBUtil.createEntityManager();
 
-		Report r = em.find(Report.class, Integer.parseInt(request.getParameter("id")));
+        Report r = em.find(Report.class, Integer.parseInt(request.getParameter("id")));
 
-		em.close();
+        em.close();
 
-		Employee login_employee = (Employee)request.getSession().getAttribute("login_employee");
-		if(login_employee.getId() == r.getEmployee().getId()) {
-		request.setAttribute("report", r);
-		request.setAttribute("_token", request.getSession().getId());
-		request.getSession().setAttribute("report_id", r.getId());
-	}
+        Employee login_employee = (Employee)request.getSession().getAttribute("login_employee");
+        if(login_employee.getId() == r.getEmployee().getId()) {
+            request.setAttribute("report", r);
+            request.setAttribute("_token", request.getSession().getId());
+            request.getSession().setAttribute("report_id", r.getId());
+        }
 
-	RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/reports/edit.jsp");
-    rd.forward(request, response);
-	}
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/reports/edit.jsp");
+        rd.forward(request, response);
+    }
+
 }
